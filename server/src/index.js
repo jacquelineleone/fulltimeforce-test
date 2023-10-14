3e7406b871c71bb99fc0e506f9006a60df922baf
jacquelineleone <jacqueline.leone02@gmail.com>
2023-10-14 17:39:03 -0300

    Server config

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
+app.use(express.json());
+
+async function main() {
+  try {
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on http://localhost:" + process.env.PORT);
+    });
+  } catch (error) {
+    console.error("Error starting application:", error);
+  }
+}
+
+main();
