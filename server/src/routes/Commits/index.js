4a64c69c7e6a7ba6634436e6b9da53c77cb4794c
jacquelineleone <jacqueline.leone02@gmail.com>
2023-10-16 14:34:06 -0300

    Route change

--- server/src/routes/Commits/index.js
+++ server/src/routes/Commits/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getCommits } = require("../../controllers/Commits");
 const router = express.Router();
 
-router.get("/commits", getCommits);
+router.post("/commits", getCommits);
 
 module.exports = router;
